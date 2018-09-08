921cb361587ec41dfb32263515d79e0bb11125f6
Praveen Darshanam <pdarshanam@ptc.com>
2018-09-08 04:39:34 +0100

    architecture change

--- plugins/gardener/gardener.py
+++ plugins/gardener/gardener.py
@@ -1,21 +1,54 @@
 #!/usr/bin/env python
 
 import sys,os
-import argparse
+import subprocess
+
+def get_values(env):
+  val = env.split(" ")
+  # count: 1 val=cluster-info, 2 val=get ns, 3 val=get po -n dev
+  return val
+
+def exec_command(val_list, kctl, kube_ctx):
+  if len(val_list) == 3:
+    if "--all-namespaces" in val_list[2]:
+      cmd = kctl + " " + val_list[0] + " " + val_list[1] + " " + val_list[2]
+    else:
+      cmd = kctl + " " + val_list[0] + " " + val_list[1] + " -n " + val_list[2]
+  elif len(val_list) == 2:
+    cmd = kctl + " " + val_list[0] + " " + val_list[1]
+  elif len(val_list) == 1:
+    cmd = kctl + " " + val_list[0]
+  else:
+    print "invalid [verb, resource, namespace]:", val_list
+  print "cmd:", cmd
+  #proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+  #out, err = proc.communicate()
 
 def main():
-  #print "kubectl path:", os.environ.get('KUBECTL_PLUGINS_CALLER')
+  kctl = os.environ.get('KUBECTL_PLUGINS_CALLER')
   #print "plugin name:", os.environ.get('KUBECTL_PLUGINS_DESCRIPTOR_NAME')
   #print "command file:", os.environ.get('KUBECTL_PLUGINS_DESCRIPTOR_COMMAND')
-  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_GARDENS'):
-    print "garden list"
+  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_GARDEN'):
+    cmd = "gardenctl target garden msa-dev-garden"
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    kctx, err = proc.communicate()
+    if not err:
+      print "kctx:", kctx
+    k8s_val = get_values(os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_GARDEN'))
+    exec_command(k8s_val, kctl, kctx)
+    print "k8s_val:", k8s_val
+    # gardenctl target garden msa-dev-garden |cut -d = -f2
     print os.system("gardenctl ls gardens")
   
-  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SEEDS'):
+  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SEED'):
+    k8s_val = get_values(os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SEED'))
+    print "k8s_val:", k8s_val
     print "seeds list"
     print os.system("gardenctl ls seeds")
   
-  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SHOOTS'):
+  if os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SHOOT'):
+    k8s_val = get_values(os.environ.get('KUBECTL_PLUGINS_LOCAL_FLAG_SHOOT'))
+    print "k8s_val:", k8s_val
     print "shoots list"
     print os.system("gardenctl ls shoots")
 
